6eb10395cfe52bf2477b56078b81b5919ce3b697
Dylan McDonald <dmcdonald21@cnm.edu>
2018-12-12 19:34:19 -0800

    replaced lorem ipsum with an AJAX call

--- frontend/site/shopping-list.js
+++ frontend/site/shopping-list.js
@@ -1,3 +1,21 @@
 $(document).ready(() => {
-	fetch("/api").then(reply => reply.json()).then(result => console.log(result));
+	fetch("/api").then(reply => reply.json()).then(result => {
+		console.log(result);
+		let output = "";
+		if(result.status !== 200) {
+			output = "<div class=\"alert alert-danger\"><strong>Oh snap!</strong> " + result.message + "</div>";
+		} else if(result.data.length === 0) {
+			output = "<div class=\"alert alert-info\"><strong>Awww!</strong> The shopping list is empty.</div>";
+		} else {
+			result.data.map(shoppingList => {
+				output = output + "<div class=\"card mb-3\">" +
+					"<div class=\"card-body\">" +
+					"<h5 class=\"card-title\">" + shoppingList.shoppingListItem + "</h5>" +
+					"<p class=\"card-text\">Quantity: " + shoppingList.shoppingListQuantity + "</p>" +
+					"</div>" +
+					"</div>";
+			});
+		}
+		$("#outputArea").html(output);
+	});
 });
